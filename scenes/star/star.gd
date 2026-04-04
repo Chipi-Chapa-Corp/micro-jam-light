b913cacb3765036e88b6612e981fc7c1b00c82de
KRASAV4EK <iliaaa.timofeev@gmail.com>
2026-04-04 17:24:47 +0200

    feat: star collecting with sound

--- scenes/star/star.gd
+++ scenes/star/star.gd
@@ -1,14 +1,23 @@
-extends Node
+extends Area2D
 
 @onready var anim: AnimatedSprite2D = $AnimatedSprite2D
-
+@onready var sfx_collect = $sfxCollect
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	anim.play("idle")
-	pass # Replace with function body.
 
+func _on_body_entered(body: Node) -> void:
+	print("gay")
+	if body.is_in_group("player"):
+		GlobalState.record_star()
+		sfx_collect.play()
+		anim.hide()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_sfx_collect_finished() -> void:
+	queue_free()
