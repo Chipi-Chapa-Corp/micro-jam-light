afba57db850b53e496537201cd4975cc75d5d299
KRASAV4EK <iliaaa.timofeev@gmail.com>
2026-04-04 20:39:58 +0200

    fix: star picking sound n-times

--- scenes/star/star.gd
+++ scenes/star/star.gd
@@ -3,21 +3,21 @@ extends Area2D
 @onready var anim: AnimatedSprite2D = $AnimatedSprite2D
 @onready var sfx_collect = $sfxCollect
 
+var collected: bool = false
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	anim.play("idle")
 
 func _on_body_entered(body: Node) -> void:
-	print("gay")
+	if collected:
+		return
+		
 	if body.is_in_group("player"):
+		collected = true
 		GlobalState.record_star()
 		sfx_collect.play()
 		anim.hide()
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
 
 func _on_sfx_collect_finished() -> void:
 	queue_free()
