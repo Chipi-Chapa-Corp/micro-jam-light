e9b7af30e575b707cd573426143bbb118b2da746
Artem Prokop <artem13.prokop@gmail.com>
2026-04-05 22:06:37 +0200

    feat: basic tutorial

--- ui/gui-elements/hint.gd
+++ ui/gui-elements/hint.gd
@@ -1,14 +1,23 @@
 extends Control
 
 @export_multiline var hint_text := "Put your hint text here"
+@export var dynamic_tutorial_mode := false
 
 @export var hover_icon: Control
 @export var bubble: Control
 @export var label: Label
 
+const TUTORIAL_HINTS := [
+	"Use A and D to move",
+	"Use W or Space to jump",
+	"There is only One Light Source which creates Shadows. Control its position using Arrow Keys",
+	"Shadows are Solid, you can stand on them or make them push you up. Too easy? Try collecting all the Stars.",
+]
+
 var bubble_pos: Vector2
 var tween: Tween
 var pulse_tween: Tween
+var tutorial_step := 0
 
 func _ready() -> void:
 	label.text = hint_text
@@ -16,10 +25,31 @@ func _ready() -> void:
 	bubble.visible = false
 	bubble.mouse_filter = Control.MOUSE_FILTER_IGNORE
 
+	if dynamic_tutorial_mode:
+		set_process(true)
+		hover_icon.visible = true
+		_set_tutorial_step(0)
+		_show_bubble_immediately()
+		start_pulse()
+		return
+
+	set_process(false)
 	hover_icon.mouse_entered.connect(_on_hover_entered)
 	hover_icon.mouse_exited.connect(_on_hover_exited)
 
 	start_pulse()
+
+
+func _process(_delta: float) -> void:
+	if not dynamic_tutorial_mode:
+		return
+
+	if tutorial_step == 0 and _is_move_pressed():
+		_advance_tutorial_step()
+	elif tutorial_step == 1 and Input.is_action_just_pressed("jump"):
+		_advance_tutorial_step()
+	elif tutorial_step == 2 and _is_shadow_control_pressed():
+		_advance_tutorial_step()
 
 func _on_hover_entered() -> void:
 	open_hint()
@@ -68,3 +98,41 @@ func stop_pulse() -> void:
 	if pulse_tween:
 		pulse_tween.kill()
 	hover_icon.scale = Vector2.ONE
+
+
+func _set_tutorial_step(step: int) -> void:
+	tutorial_step = clampi(step, 0, TUTORIAL_HINTS.size() - 1)
+	label.text = TUTORIAL_HINTS[tutorial_step]
+	if dynamic_tutorial_mode:
+		_show_bubble_immediately()
+
+
+func _advance_tutorial_step() -> void:
+	if tutorial_step >= TUTORIAL_HINTS.size() - 1:
+		set_process(false)
+		return
+
+	_set_tutorial_step(tutorial_step + 1)
+	if tutorial_step >= TUTORIAL_HINTS.size() - 1:
+		set_process(false)
+
+
+func _is_move_pressed() -> bool:
+	return Input.is_action_just_pressed("move_left") or Input.is_action_just_pressed("move_right")
+
+
+func _is_shadow_control_pressed() -> bool:
+	return (
+		Input.is_action_just_pressed("shadow_left")
+		or Input.is_action_just_pressed("shadow_right")
+		or Input.is_action_just_pressed("shadow_up")
+		or Input.is_action_just_pressed("shadow_down")
+	)
+
+
+func _show_bubble_immediately() -> void:
+	if tween:
+		tween.kill()
+	bubble.visible = true
+	bubble.position = bubble_pos
+	bubble.modulate.a = 1.0
